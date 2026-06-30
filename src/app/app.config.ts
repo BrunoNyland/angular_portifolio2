47595871d4554f19a1b912a6883c9b452ccc782a
Bruno <bruno-nyland@hotmail.com>
2026-06-30 12:35:42 -0400

    Revert "feat: adicionar suporte a hash routing para GitHub Pages e configurar ambientes"
    This reverts commit 94ef9d6cf46d5a521b946f09b0d84ac4ac7f332d.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
-import { environment } from '../environments/environment';
-
-const routerProviders = environment.useHashRouting
-  ? [
-      provideRouter(
-        routes,
-        withInMemoryScrolling({ scrollPositionRestoration: 'disabled' }),
-        withHashLocation(),
-      ),
-    ]
-  : [provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'disabled' }))];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    ...routerProviders,
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'disabled' })),
     provideHttpClient(withFetch()),
   ],
 };
